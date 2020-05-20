e57e6313943e2a90d9ad064c1a5439db6f9e045b
이수진 <jmakeaz@gmail.com>
2020-05-20 18:09:44 +0900

    Add babel, typescript, webpack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  // enntry file
+  entry: ['@babel/polyfill', './src/js/main.ts'],
+  // 컴파일 + 번들링된 js 파일이 저장될 경로와 이름 지정
+  output: {
+    path: path.resolve(__dirname, 'dist/js'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)$/,
+        include: [
+          path.resolve(__dirname, 'src/js')
+        ],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-typescript'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  // https://webpack.js.org/concepts/mode/#mode-development
+  mode: 'development'
+};
